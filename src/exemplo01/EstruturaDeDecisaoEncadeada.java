eb501e23b0e62360376be849c5241c3d2fc50140
51115152017.2 <51115152017.2@51DE194255.senacrjedu.local>
2018-05-08 21:55:59 -0300

    aula03

---
+++ src/exemplo01/EstruturaDeDecisaoEncadeada.java
@@ -0,0 +1,31 @@
+package exemplo01;
+
+public class EstruturaDeDecisaoEncadeada {
+	public static void main (String[] args) {
+		
+		/**
+		 * Estrutura de decisão
+		 * if()
+		 * if() e else
+		 * if(), else if()
+		 * if(), else if(), else
+		 **/
+		
+		/* Segundo exemplo */
+		
+		String materia = "Java";
+		
+		if(materia == "PHP") {			
+		
+			System.out.println("Disciplina do 4º modulo");
+			
+		} else if (materia == "Nanco de dados") {
+			System.out.println("Disciplina do 3º modulo");
+		} else if(materia == "Java") {
+			System.out.println("Disciplina do 5º modulo");
+		} else {
+			System.out.println("Disciplina não é dos modulos de programação");
+		}
+	}
+}
+
